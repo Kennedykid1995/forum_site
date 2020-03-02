473ae03468f8f88cc3678d9b13b1c4836d5d7587
Kennedykid1995 <34872885+Kennedykid1995@users.noreply.github.com>
2020-03-02 12:30:35 -0600

    working on the migrations

--- BackEnd/data/migrations/20200302120125_forum.js
+++ BackEnd/data/migrations/20200302120125_forum.js
@@ -11,18 +11,32 @@ exports.up = function(knex) {
   })
   knex.schema.createTable('post', post => {
       post.increment(); 
+      forum.integer('forum_id')
+      .unsigned()
+      .notNullable()
+      .refrences('id')
+      .inTable('forum')
       post.string('title', 50)
       .notNullable()
       post.text('content', 300)
       .notNullable()
+      //will be adding a integer that takes in user id
   })
   knex.schema.createTable('comment', comment => {
       comment.increament();
+      forum.integer('post_id')
+      .unsigned()
+      .refrences('forum_id')
+      .inTable('post')
       comment.string('content', 300)
       .notNullable()
+    //will be adding a integer that takes in user id
   })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+  .dropTableIfExists('forum')
+  .dropTableIfExists('post')
+  .dropTableIfExists('comment')
 };
