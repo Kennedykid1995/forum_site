371373b3475c45bfbc639422162612598ca50b30
Kennedykid1995 <34872885+Kennedykid1995@users.noreply.github.com>
2020-03-02 14:20:25 -0600

    working on server

--- BackEnd/API/server.js
+++ BackEnd/API/server.js
@@ -25,22 +25,67 @@ server.get('/:title', (req, res) => {
     db('forum', 'post')
     .where({id}, {forum_id})
     .then(forum => res.status(200).json(forum))
+    .catch(err => res.satus(200).json(err))
+})
+server.get('/:title', (req, res) => {
+    const {forum_id} = req.params;
+    db('post')
+    .where({forum_id})
     .then(post => res.status(200).json(post))
     .catch(err => res.satus(200).json(err))
 })
-//One Post within Forum All comments on Post
+//One Post within Forum - All comments on Post
 server.get('/:title/:post_title', (req, res) => {
     const {forum_id} = req.params;
-    const {post_id} = req.params;
-    db('post', 'comments')
+    db('post')
     .where({forum_id}, {post_id})
     .then(post => res.status(200).json(post))
+    .catch(err => res.status(500).json(err))
+})
+server.get('/:title/:post_title', (req, res) => {
+    const {post_id} = req.params;
+    db('comments')
+    .where({post_id})
     .then(comment => res.status(200).json(comment))
     .catch(err => res.status(500).json(err))
 })
 
 //POST
 //Forum
+server.post('/frontpage', (req, res) => {
+    const forum = req.body;
+    const {title} = req.body; 
+    const {description} = req.body;
+    if(!title & !description){
+        res
+        .status(400)
+        .json({errorMessage: "NEED TITLE/DESCRIPTION"})
+    }
+    console.log(forum)
+    db.insert(forum)
+        .into('forum')
+        .then(ids => {
+            res.status(201).json(ids);
+        })
+        .catch(err => res.status(500).json(err))
+})
+//create a post onto a forum
+server.post('/:title', (req, res) => {
+    const post = req.body;
+    const {post_title} = req.body; 
+    const {post_content} = req.body;
+    if(!post_title & !post_content){
+        res
+        .status(400)
+        .json({errorMessage: "NEED TITLE/DESCRIPTION"})
+    }
+    db.insert(post)
+        .into('post')
+        .then(ids => {
+            res.status(201).json(ids);
+        })
+        .catch(err => res.status(500).json(err))
+})
 //Post
 //Comment
 
