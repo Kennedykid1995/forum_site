662729ef483c33f2875ebd8f02843ad292beddeb
Kennedykid1995 <34872885+Kennedykid1995@users.noreply.github.com>
2020-03-03 12:41:42 -0600

    working on POST comment

--- BackEnd/API/server.js
+++ BackEnd/API/server.js
@@ -23,66 +23,51 @@ server.get('/frontpage', async (req, res) => {
 })
 //One Forum - All Posts in a Forum
 server.get('/:id', async (req, res) => {
-    // const { id } = req.params;
-    // db('forums')
-    //     .where({ id })
-    //     .then(forum => res.status(200).json(forum))
-    //     .catch(err => res.status(500).json(err))
-    try{
+    try {
         const { id } = req.params;
-        const data = await db('forums').where({id});
+        const data = await db('forums').where({ id });
         res.json(data);
-    } catch(err){
-        res.status(500).json(err); 
+    } catch (err) {
+        res.status(500).json(err);
     }
-    
-})
 
-server.get('/:id', (req, res) => {
-    const { id } = req.params;
-    db('forum_post')
-        .where({ id })
-        .send("POSTS")
-        .then(post => res.status(200).json(post))
-        .catch(err => res.status(500).json(err))
 })
 // One Post within Forum - All comments on Post
 
 //POST
 //Forum
-server.post('/frontpage', (req, res) => {
-    const forum = req.body;
-    const { title } = req.body;
-    const { description } = req.body;
-    if (!title & !description) {
-        res
-            .status(400)
-            .json({ errorMessage: "NEED TITLE/DESCRIPTION" })
+server.post('/frontpage', async (req, res) => {
+    try {
+        const forum = req.body;
+        const { title, description } = req.body;
+        const create = await db.insert(forum).into('forums')
+        if (!title & !description) {
+            res.status(400).json({ errorMessage: "NEED TITLE/DESCRIPTION" })
+        } else res.status(201).json(create);
+    } catch (err) {
+        res.status(500).json(err);
     }
-    console.log(forum)
-    db.insert(forum)
-        .into('forums')
-        .then(ids => {
-            res.status(201).json(ids);
-        })
-        .catch(err => res.status(500).json(err))
 })
+
 //create a post onto a forum
-server.post('/:title', (req, res) => {
-    const post = req.body;
-    const { post_title } = req.body;
-    const { post_content } = req.body;
-    if (!post_title & !post_content) {
-        res
-            .status(400)
-            .json({ errorMessage: "NEED TITLE/DESCRIPTION" })
+
+server.post('/:id', async (req, res) => {
+    try {
+        const forum = await db('forums')
+        const post = req.body;
+        const { post_title, post_content } = req.body;
+        const create = await db.insert(post).into('forum_post');
+        const intoForum = await db.insert(create).into(forum);
+        if (!post_title & !post_content) {
+            res.status(400).json({ errorMessage: "NEED TITLE/CONTENT!" })
+        } else {
+            console.log(create);
+            res.status(200).json(intoForum)
+        }
+    } catch (err) {
+        console.log(err, req.params); 
+        res.status(500).json(err)
     }
-    db.insert(post)
-        .into('post')
-        .then(ids => {
-            res.status(201).json(ids);
-        })
-        .catch(err => res.status(500).json(err))
 })
 //Post
 //Comment
