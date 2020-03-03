d2b8cd3a1164a18aca3af5b481a6fb29770c80db
Kennedykid1995 <34872885+Kennedykid1995@users.noreply.github.com>
2020-03-03 11:34:09 -0600

    using async await instead of other way

--- BackEnd/API/server.js
+++ BackEnd/API/server.js
@@ -13,42 +13,28 @@ server.get('/', (req, res) => {
 
 //GET: 
 //All Forums
-server.get('/frontpage', (req, res) => {
-    db('forums')
-    .then(forum => res.status(200).json(forum))
-    .catch(err => res.status(500).json(err));
+server.get('/frontpage', async (req, res) => {
+    const data = await db('forums')
+    res.json(data);
 })
 //One Forum - All Posts in a Forum
-server.get('/:title', (req, res) => {
+server.get('/:id', (req, res) => {
     const {id} = req.params; 
-    const {forum_id} = req.params;
-    db('forum', 'post')
-    .where({id}, {forum_id})
+    db('forums')
+    .where({id})
     .then(forum => res.status(200).json(forum))
-    .catch(err => res.satus(200).json(err))
+    .catch(err => res.status(500).json(err))
 })
-server.get('/:title', (req, res) => {
-    const {forum_id} = req.params;
-    db('post')
-    .where({forum_id})
-    .then(post => res.status(200).json(post))
-    .catch(err => res.satus(200).json(err))
-})
-//One Post within Forum - All comments on Post
-server.get('/:title/:post_title', (req, res) => {
-    const {forum_id} = req.params;
-    db('post')
-    .where({forum_id}, {post_id})
+
+server.get('/:id', (req, res) => {
+    const {id} = req.params;
+    db('forum_post')
+    .where({id})
+    .send("POSTS")
     .then(post => res.status(200).json(post))
     .catch(err => res.status(500).json(err))
 })
-server.get('/:title/:post_title', (req, res) => {
-    const {post_id} = req.params;
-    db('comments')
-    .where({post_id})
-    .then(comment => res.status(200).json(comment))
-    .catch(err => res.status(500).json(err))
-})
+// One Post within Forum - All comments on Post
 
 //POST
 //Forum
