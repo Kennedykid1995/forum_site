2f5f314ccf1d21e21b468b64746e65f34a5b231d
Kennedykid1995 <34872885+Kennedykid1995@users.noreply.github.com>
2020-03-04 15:46:18 -0600

    working on the frontend, react redux is what is being used

---
+++ FrontEnd /forum/src/actions/actions.js
@@ -0,0 +1,13 @@
+import axios from 'axios'; 
+
+export const ERROR = 'ERROR';
+export const GETTINGFORUMS = 'GETTINGFORUMS';
+export const GETTINGPOSTS = 'GETTINGPOSTS';
+export const GETTINGCOMMENTS = 'GETTINGCOMMENTS';
+export const GETTINGFORUM = 'GETTINGFORUM';
+export const GETTINGPOST = 'GETTINGPOST';
+
+const forumsURL = ""; //all forums
+const forumURL = ""; //single forum all posts
+const postURL = ""; //single post all comments 
+
---
+++ FrontEnd /forum/src/reducers/reducers.js
@@ -0,0 +1,58 @@
+import * as actionTypes from '../actions/actions';
+
+const initialState = {
+    forums: [],
+    posts: [],
+    comments: [],
+    forum: {},
+    post: {},
+    gettingForums: false,
+    gettingPosts: false,
+    gettingComments: false,
+    gettingForum: false,
+    gettingPost: false,
+    error: null
+};
+
+export const siteReducer = (state = initialState, action) => {
+    switch (action.type) {
+
+        case actionTypes.GETTINGFORUMS:
+            return { ...state, gettingForums: true }
+        case actionTypes.GETFORUMS:
+            return { ...state, forums: action.payload, gettingForums: false }
+
+        case actionTypes.GETTINGPOSTS:
+            return { ...state, gettingPosts: true }
+        case actionTypes.GETPOSTS:
+            return { ...state, posts: action.payload, gettingPosts: false }
+
+        case actionTypes.GETTINGCOMMENTS:
+            return { ...state, gettingComments: true }
+        case actionTypes.GETCOMMENTS:
+            return { ...state, comments: action.payload, gettingComments: false }
+
+        case actionTypes.GETTINGFORUM:
+            return { ...state, gettingForum: true }
+        case actionTypes.GETFORUM:
+            return { ...state, forum: action.payload, gettingForum: false }
+
+        case actionTypes.GETTINGPOST:
+            return { ...state, gettingPost: true }
+        case actionTypes.GETPOST:
+            return { ...state, post: action.payload, gettingPost: false }
+
+        case actionTypes.ERROR:
+            return {
+                ...state,
+                gettingForums: false,
+                gettingPosts: false,
+                gettingComments: false,
+                gettingForum: false,
+                gettingPost: false,
+                error: action.payload,
+            };
+        default:
+            return state
+    }
+}
