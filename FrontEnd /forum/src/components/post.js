9ea6f789937d4825821f3e1436d35e5610c5922c
Kennedykid1995 <34872885+Kennedykid1995@users.noreply.github.com>
2020-03-12 14:32:54 -0500

    working on implenting material ui

--- FrontEnd /forum/src/components/post.js
+++ FrontEnd /forum/src/components/post.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react'
 import {getPost} from '../actions/actions'; 
 import {connect} from 'react-redux'; 
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+
+const useStyles = makeStyles({
+    root: {
+        minWidth: 275,
+    },
+    title: {
+        fontSize: 14, 
+    },
+    content: {
+        fontSize: 12,
+    }
+})
+
 
 class Post extends Component {
     componentDidMount(){
         this.props.getPost();
     }
     render() {
+        const classes = useStyles(); 
         const postData = this.props.post
         return (
             <div>
@@ -19,7 +40,7 @@ class Post extends Component {
                                 {postData[key].map((data) => {
                                     return(
                                         <div>
-                                            <span key={data.id}>{data.post_title} </span>
+                                            <Typography className={classes.title} key={data.id}>{data.post_title} </Typography>
                                             <span>{data.post_content}</span>
                                             <section>
                                                 <span>{data.comment_content}</span>
